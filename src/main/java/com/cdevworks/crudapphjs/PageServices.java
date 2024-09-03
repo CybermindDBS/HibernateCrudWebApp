22359e8dcd7cd75b21fa383a28be3913a40f7a74
ShakthyBalan <shakthybalan.dbs@gmail.com>
2024-09-03 16:36:56 +0530

    Update PageServices.java

--- src/main/java/com/cdevworks/crudapphjs/PageServices.java
+++ src/main/java/com/cdevworks/crudapphjs/PageServices.java
@@ -3,8 +3,15 @@ public class PageServices
     public void page1Get(HttpServletRequest request, HttpServletResponse response)
     {
       request.setAttribute("pageName", "Hibernate | Single Row Operations");
-      request.setAttribute("tables", List.of(new Table(), new Table()));
+      List<List<String>> table1 = new ArrayList();
+      table1.add(Arrays.asList("a1", "a2", "a3"));
+      table1.add(Arrays.asList("a4", "a5", "a6"));
+      List<List<String>> table2 = new ArrayList();
+      table1.add(Arrays.asList("b1", "b2", "b3"));
+      table1.add(Arrays.asList("b4", "b5", "b6"));
+      request.setAttribute("tables", List.of(new Table("Table A", table1), new Table("Table B", table2)));
     }
+    
     public void page1Post(HttpServletRequest request, HttpServletResponse response)
     {
       
@@ -14,6 +21,7 @@ public class PageServices
     {
       
     }
+    
     public void page1Post(HttpServletRequest request, HttpServletResponse response)
     {
       
