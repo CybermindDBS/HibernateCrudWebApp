9f1a0b38d0309e19999b3c4d6727f5376688fa7e
ShakthyBalan <shakthybalan.dbs@gmail.com>
2024-09-03 15:26:13 +0530

    Update Table.java

--- src/main/java/com/cdevworks/crudapphjs/Table.java
+++ src/main/java/com/cdevworks/crudapphjs/Table.java
@@ -2,4 +2,10 @@ public class Table
 {
   String tableName;
   List<ArrayList> rows;
+
+  Table(String tableName, List<ArrayList> rows)
+  {
+    this.tableName = tableName;
+    this.rows = rows;
+  }
 }
